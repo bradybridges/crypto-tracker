97f8e22702c97229db9390ab7b98f2e6bafe593d
Brady Bridges <bradyjbridges@gmail.com>
2019-11-04 16:44:33 -0700

    Updates

---
+++ precache-manifest.c576db5616b98743bf17090bf287aa75.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35b7f81ed408a7528a11fe0b29ba38e",
+    "url": "/crypto-tracker/index.html"
+  },
+  {
+    "revision": "50496ec0302658a9e171",
+    "url": "/crypto-tracker/static/css/main.8f1badde.chunk.css"
+  },
+  {
+    "revision": "da28b6c4c58a928ccc98",
+    "url": "/crypto-tracker/static/js/2.847f80a8.chunk.js"
+  },
+  {
+    "revision": "50496ec0302658a9e171",
+    "url": "/crypto-tracker/static/js/main.aee0947a.chunk.js"
+  },
+  {
+    "revision": "907b699aaee8fdbafbc5",
+    "url": "/crypto-tracker/static/js/runtime-main.9907b982.js"
+  },
+  {
+    "revision": "7be4698ba173f10dda790d1c8a3f8d2b",
+    "url": "/crypto-tracker/static/media/30 Black abstract Wallpapers HD Download.7be4698b.jpg"
+  },
+  {
+    "revision": "71ee38013c6452e8cc668384188dbddb",
+    "url": "/crypto-tracker/static/media/close-button.71ee3801.svg"
+  },
+  {
+    "revision": "33bc766e122306c2d81757fd82ae114d",
+    "url": "/crypto-tracker/static/media/home.33bc766e.svg"
+  },
+  {
+    "revision": "b272803d36f912ab25002e9366d98f43",
+    "url": "/crypto-tracker/static/media/left-arrow.b272803d.svg"
+  },
+  {
+    "revision": "da1c4f1fc98e7cefa532aa8f1606de6b",
+    "url": "/crypto-tracker/static/media/portfolio.da1c4f1f.svg"
+  },
+  {
+    "revision": "fcc346357efa63603e3dfaa46ab246d9",
+    "url": "/crypto-tracker/static/media/search.fcc34635.svg"
+  }
+]);
